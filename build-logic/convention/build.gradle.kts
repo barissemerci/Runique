ffb10a9faa4cacd840b526628441818abf377970
Barış SEMERCİ <barissemerci3@gmail.com>
2024-10-30 16:20:09 +0300

    add JvmKtorConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -40,5 +40,9 @@ gradlePlugin {
             id = "runique.jvm.library"
             implementationClass = "JvmLibraryConventionPlugin"
         }
+        register("jvmKtor") {
+            id = "runique.jvm.ktor"
+            implementationClass = "JvmKtorConventionPlugin"
+        }
     }
 }
