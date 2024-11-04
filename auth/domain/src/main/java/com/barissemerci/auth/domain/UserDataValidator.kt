d06b00f6fd8c7360c742b6394213342bb75c1104
Barış SEMERCİ <barissemerci3@gmail.com>
2024-11-04 13:25:31 +0300

    add validating user data

--- auth/domain/src/main/java/com/barissemerci/auth/domain/UserDataValidator.kt
+++ auth/domain/src/main/java/com/barissemerci/auth/domain/UserDataValidator.kt
@@ -1,7 +1,25 @@
 package com.barissemerci.auth.domain
 
-class UserDataValidator {
-    companion object{
+class UserDataValidator(private val patternValidator: PatternValidator) {
+    fun isValidEmail(email: String): Boolean {
+        return patternValidator.matches(email.trim())
+    }
+
+    fun validatePassword(password: String): PasswordValidationState {
+        val hasMinLength = password.length >= MIN_PASSWORD_LENGTH
+        val hasDigit = password.any { it.isDigit() }
+        val hasUpperCaseCharacter = password.any { it.isUpperCase() }
+        val hasLowerCaseCharacter = password.any { it.isLowerCase() }
+        return PasswordValidationState(
+            hasMinimumLength = hasMinLength,
+            hasNumber = hasDigit,
+            hasUppercaseCharacter = hasUpperCaseCharacter,
+            hasLowercaseCharacter = hasLowerCaseCharacter,
+        )
+
+    }
+
+    companion object {
         const val MIN_PASSWORD_LENGTH = 9
     }
 }
